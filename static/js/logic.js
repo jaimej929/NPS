dd9a45f3b0858644309070ee0bba7399b508bfe9
Lisa Stroh <lisb020@yahoo.com>
2021-03-09 20:01:21 -0800

    Need to figure out layer groups

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,19 +1,28 @@
-// let myMap = L.map('map', {
-//     center: [39.8283, -98.5795],
-//     zoom: 5
-// });
-// Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
-function createMap(NationalParks) {
-    let normalmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+
+var allData = [];
+let normalmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
         maxZoom: 18,
         zoomOffset: -1,
         id: "mapbox/streets-v11",
         accessToken: API_KEY
+});
+let myMap = L.map('map', {
+  center: [39.8283, -98.5795],
+  zoom: 4,
+  layers: [normalmap]
   });
 
+baseMaps = {};
+overlayMaps = {};
+
+// 17 week 1 activity 9
+let mylayers = L.control.layers(baseMaps, overlayMaps, {
+    collapsed: true
+}).addTo(myMap);
+
+function createMap(NationalParks) {
   let baseMaps = {
       "View Map": normalmap
   };
@@ -21,37 +30,35 @@ function createMap(NationalParks) {
   let overlayMaps = {
       "National Parks": NationalParks
   };
-  let myMap = L.map('map', {
-    center: [39.8283, -98.5795],
-    zoom: 5,
-    layers: [normalmap, NationalParks]
-  });
-  L.control.layers(baseMaps, overlayMaps, {
-      collapsed: true
-  }).addTo(myMap);
+  
+  myMap.layers = [normalmap, NationalParks];
 }
 
+function createdropdown(data) {
+  //load dropdown menu with activities
+  data.forEach(name => d3.select("#selActivity").append("option").text(name.activities_name).attr("value",name.id));
+}
+function selectdropdown(activityid) {
+    //create empty lists to hold parkCode and parks
+    let parkCode = [];
+    let parks = [];
 
-// var marker = L.marker([39.52, -98.67], {
-//     draggable: true,
-//     title: "My First Marker"
-//   }).addTo(myMap);
-
-//   marker.bindPopup("<h1>" + "Random Mark for futre stuff" + "</h1>");
-
-//   let layers = {
-//     COMING_SOON: new L.LayerGroup(),
-//     EMPTY: new L.LayerGroup(),
-//     LOW: new L.LayerGroup(),
-//     NORMAL: new L.LayerGroup(),
-//     OUT_OF_ORDER: new L.LayerGroup()
-//   };
-
-  function createMarkers(response) {
-
+    //loop through park_activities list and match the activity ids from the selected activity in the dropdown
+    allData.park_activities.forEach(function(name) {
+      if (name.id == activityid) {
+        parkCode.push(name.park_code);
+      }
+    })
     
-    let parks = response.data.park;
-    console.log(parks);
+    //loop through parkcode to match and get park info
+    for (code of parkCode) {
+      for (park of allData.park) {
+        if (code == park.park_code) {
+          parks.push(park);
+          break;
+        }
+      }
+    }
   
     // Initialize an array to hold park markers
     var parkMarkers = [];
@@ -63,12 +70,38 @@ function createMap(NationalParks) {
     //   console.log(npark.image_url);
       let image = npark.image_url;
     
+      // For each park, create a marker and bind a popup with the parks's name and add picture
+      var parkMarker = L.marker([npark.lat, npark.long])
+        .bindPopup("<h3>" + npark.park_name + "</h3>" + "<img src='" + image + "'" + "class=popupImage " + "/>");
+        //console.log(image);
+  
+      // Add the marker to the parkMarkers array
+      parkMarkers.push(parkMarker);
+    }
+    createMap(L.layerGroup(parkMarkers))
 
+};
+
+  function createMarkers(response) {
+
+    allData = response.data;
+    let parks = response.data.park;
+    //console.log(parks);
+  
+    // Initialize an array to hold park markers
+    var parkMarkers = [];
+  
+    // Loop through the parks array
+    for (var index = 0; index < parks.length; index++) {
+      var npark = parks[index];
+    //   console.log(npark);
+    //   console.log(npark.image_url);
+      let image = npark.image_url;
   
       // For each park, create a marker and bind a popup with the parks's name and add picture
       var parkMarker = L.marker([npark.lat, npark.long])
         .bindPopup("<h3>" + npark.park_name + "</h3>" + "<img src='" + image + "'" + "class=popupImage " + "/>");
-        console.log(image);
+        //console.log(image);
   
       // Add the marker to the parkMarkers array
       parkMarkers.push(parkMarker);
@@ -76,7 +109,7 @@ function createMap(NationalParks) {
   
     // Create a layer group made from the park markers array, pass it into the createMap function
     createMap(L.layerGroup(parkMarkers));
-
+    createdropdown(response.data.activities)
   }
   
   
@@ -84,4 +117,3 @@ function createMap(NationalParks) {
 
   axios.get('http://localhost:5000/')
   .then(createMarkers);
-//   shelter1.bindPopup("<img src='" + picURL2 + "'" + " class=popupImage " + "/>");
