9268783978897bd087965fa6a00b25fd97657b82
Lisa Stroh <lisb020@yahoo.com>
2021-03-11 20:33:22 -0800

    Delete config.js

--- static/js/config.js
+++
@@ -1,6 +0,0 @@
-var API_KEY = "pk.eyJ1IjoibGlzYjAyMCIsImEiOiJja2xsamVvbWswcWtkMm9wM3phamw5ajQ5In0.VMo77cesBy3oWelaySG_kw";
-<<<<<<< HEAD
-var WEATHER_KEY = "c47972982cc3410e0b4e9c102c0c266d";
-=======
-var WEATHER_KEY = "04f88df63ed250bcacea2094c02483a0";
->>>>>>> ad7856bac6fe0d3e8e4fccfd885d339c5884e68a
